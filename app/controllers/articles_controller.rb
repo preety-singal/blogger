10be50bcb3a3a4488bb7480eb24766178130d0c4
preety <preetysingal@poplify.com>
2015-02-02 13:59:38 +0530

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,12 @@
+class ArticlesController < ApplicationController
+	def index
+  @articles = Article.all
+end
+def new
+	@article = Article.new
+	end
+	def show
+    @article = Article.find(params[:id])
+
+end
+end
